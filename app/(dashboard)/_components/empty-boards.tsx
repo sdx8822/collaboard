4acc89ebe4a3438ee92e09433ba8219f8e9d8090
sdx8822 <147133974+sdx8822@users.noreply.github.com>
2024-02-22 10:08:10 -0800

    create mutation for loading state; add global toaster (#4)

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,33 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { useMutation } from "convex/react";
 import Image from "next/image";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = () => {
+    if (!organization) return;
+
+    mutate({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+    .then((id) => {
+      toast.success("Board created");
+
+      // TODO: redirect to board/{id}
+    })
+    .catch(() => toast.error("Failed to create board"));
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image
@@ -11,17 +37,19 @@ export const EmptyBoards = () => {
         width={140}
       />
 
-      <h2 className="text-2xl font-semibold mt-6">
-        Create your first board!
-      </h2>
+      <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
 
       <p className="text-muted-foreground text-sm mt-2">
         Start by creating a board for your organization
       </p>
 
       <div className="mt-6">
-        <Button size={"lg"}>
-            Create board
+        <Button
+          disabled={pending}
+          onClick={onClick}
+          size={"lg"}
+        >
+          Create board
         </Button>
       </div>
     </div>
