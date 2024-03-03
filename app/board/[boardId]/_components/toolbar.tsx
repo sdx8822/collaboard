6fd2e7c84d42531b9d2644b84de266f768b32dad
sdx8822 <stevendx302@gmail.com>
2024-03-02 22:52:44 -0800

    feat: add ToolButton component to render toolbar buttons with icons and labels

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,20 +1,76 @@
+import {
+  Circle,
+  MousePointer2,
+  Pencil,
+  Redo2,
+  Square,
+  StickyNote,
+  Type,
+  Undo2,
+} from "lucide-react";
+import { ToolButton } from "./tool-button";
+
 export const Toolbar = () => {
   return (
     <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <div>Pencil</div>
+        <ToolButton
+          label="Select"
+          icon={MousePointer2}
+          onClick={() => {}}
+          isActive={false}
+        />
 
-        <div>Square</div>
+        <ToolButton
+          label="Text"
+          icon={Type}
+          onClick={() => {}}
+          isActive={false}
+        />
 
-        <div>Circle</div>
+        <ToolButton
+          label="Sticky note"
+          icon={StickyNote}
+          onClick={() => {}}
+          isActive={false}
+        />
 
-        <div>Ellipsis</div>
+        <ToolButton
+          label="Rectangle"
+          icon={Square}
+          onClick={() => {}}
+          isActive={false}
+        />
+
+        <ToolButton
+          label="Ellipsis"
+          icon={Circle}
+          onClick={() => {}}
+          isActive={false}
+        />
+
+        <ToolButton
+          label="Pen"
+          icon={Pencil}
+          onClick={() => {}}
+          isActive={false}
+        />
       </div>
 
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <div>Undo</div>
+        <ToolButton
+          label="Undo"
+          icon={Undo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
 
-        <div>Redo</div>
+        <ToolButton
+          label="Redo"
+          icon={Redo2}
+          onClick={() => {}}
+          isDisabled={true}
+        />
       </div>
     </div>
   );
