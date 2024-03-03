034d122e555c9569fd6a4be2dba8b4cce2f97c91
sdx8822 <147133974+sdx8822@users.noreply.github.com>
2024-03-02 20:37:07 -0800

    feat: add support for Liveblocks integration in Canvas component (#14)

--- convex/board.ts
+++ convex/board.ts
@@ -1,6 +1,6 @@
 import { v } from "convex/values";
 
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 const images = [
   "/placeholders/1.svg",
@@ -162,4 +162,13 @@ export const unfavorite = mutation({
 
     return board;
   },
+});
+
+export const get = query({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const board = ctx.db.get(args.id);
+
+    return board;
+  }
 });
